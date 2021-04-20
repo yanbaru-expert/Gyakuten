eccedf7bb37d3b7a9cd25ae423c216b34f70fa49
OhtakeYusuke <o.yusuke0501@gmail.com>
2021-04-21 08:40:21 +0900

    お問い合わせ文字数制限修正

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,7 +16,7 @@ class ContactsController < ApplicationController
     end
 
     def contact_validation
-      if params[:message].length > 1000 || params[:name].length > 20 || params[:ruby_name].length > 30
+      if params[:message].length > 1000 || params[:name].length > 100 || params[:ruby_name].length > 30
         flash[:notice] = "メールの送信に失敗しました。もう一度送信をお願いします"
         redirect_to static_pages_url(anchor: "design_contact")
       end
