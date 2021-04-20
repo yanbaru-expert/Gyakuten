75a23f15b12bd23dbe79fa8ac01e90acb22f9f5a
OhtakeYusuke <o.yusuke0501@gmail.com>
2021-04-20 15:28:03 +0900

    メール機能ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
   root to: "static_pages#home"
   
   get "design", to: "static_pages#design", as: :static_pages
