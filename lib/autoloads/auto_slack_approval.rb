ce20e86254c00bbe26997886f35ac3101e3b28aa
T.Tsujii <ttakehisabt@gmail.com>
2021-04-25 17:36:54 +0900

    デザイン講座のSlackを受け付けるように修正

--- lib/autoloads/auto_slack_approval.rb
+++ lib/autoloads/auto_slack_approval.rb
@@ -1,12 +1,14 @@
 class AutoSlackApproval
-  def initialize(slack_name:, slack_id:)
+  def initialize(slack_name:, slack_id:, email:)
     @slack_name = slack_name
     @slack_id = slack_id
+    @email = email
   end
 
   def approval?
     client = Slack::Web::Client.new(token: Rails.application.credentials.dig(:slack, :oauth_token, @slack_name.to_sym))
     user = client.users_info(user: @slack_id).user
+    # @email == user&.profile&.email を後で条件に追加
     !user.deleted && !user.is_bot
   rescue Slack::Web::Api::Errors::UserNotFound
     false
