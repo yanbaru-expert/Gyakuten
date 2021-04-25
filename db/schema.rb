ebb0ba2fa3479d2de67bbac8de5e956d0683debe
OhtakeYusuke <o.yusuke0501@gmail.com>
2021-04-25 15:53:34 +0900

    デザイン講座LP内文章修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_424_061_329) do
+ActiveRecord::Schema.define(version: 2021_04_24_061329) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -171,7 +172,7 @@ ActiveRecord::Schema.define(version: 20_210_424_061_329) do
     t.inet "last_sign_in_ip"
     t.boolean "flag", default: false
     t.string "slack_id", null: false
-    t.datetime "approval_at", default: "2020-06-16 07:36:58"
+    t.datetime "approval_at", default: "2021-04-05 12:24:20"
     t.integer "slack_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
