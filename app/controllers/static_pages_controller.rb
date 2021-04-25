0126bcfd73fb9a47f329f285dbc29aedb1c52dc8
T.Tsujii <ttakehisabt@gmail.com>
2021-04-25 18:54:51 +0900

    定数をconfigに移動

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,12 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
-  # 【3-2】レスポンシブデザイン
-  SAMPLE_MOVIE_DESIGN_URL = "https://www.youtube.com/embed/uSQ2Kw6phs0"
-  # 【1-1】デザインとアートの違い(現在ツール部分代用)
-  SAMPLE_MOVIE_TOOL_URL = "https://www.youtube.com/embed/QLeskX1PQY0"
-  # 【1-3】仕組み化（マーケティング）
-  SAMPLE_MOVIE_MARKETING_URL = "https://www.youtube.com/embed/DYEek-SNurI"
-
   def home
     @faqs = Faq.where(genre: %w[always top]).order(position: :asc)
   end
 
   def design
     @faqs = Faq.where(genre: %w[always design]).order(position: :asc)
-    @movies = Movie.where(url: [SAMPLE_MOVIE_DESIGN_URL,
-                                SAMPLE_MOVIE_TOOL_URL,
-                                SAMPLE_MOVIE_MARKETING_URL]
-                              )
+    @movies = Movie.where(url: Settings.design.movie.to_h.values)
     @price_id = ENV["DESIGN_PRICE_ID"]
     @price = if Rails.env.production?
                Stripe::Price.retrieve(@price_id).unit_amount
