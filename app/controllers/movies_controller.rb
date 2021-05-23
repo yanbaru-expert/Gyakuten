e77ba4dd184822e589cd3732a18e4d19a1a4f062
tanahashi.yasuhiro <yasuhiro.tanahashi@vitalize.co.jp>
2021-05-23 13:47:51 +0900

    MovieのAjax処理

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,6 @@ class MoviesController < ApplicationController
     @code_name = Genre.valid_code_name(params[:genre])
     @genre = Genre.convert_display_name(@code_name)
     @movies = Movie.fetch_from(@code_name, params[:page])
-    @watched_movie_ids = current_user.watched_movies.pluck(:movie_id)
+    @progresses = current_user.progresses.pluck(:materiable_id)
   end
 end
