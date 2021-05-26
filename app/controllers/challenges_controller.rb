c2c319b5ca31c2a3e5a4a18c49cacd86ed3af5a7
tanahashi.yasuhiro <yasuhiro.tanahashi@vitalize.co.jp>
2021-05-26 16:37:51 +0900

    ChallengeのAjax化を実装

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -2,12 +2,7 @@ class ChallengesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @challenges = Challenge.order(id: :asc)
-    if user_signed_in?
-      @progresses = current_user.progresses.where(materiable_type: "Challenge").pluck(:materiable_id)
-
-      # @complete_challenge_ids = current_user.complete_challenges.pluck(:challenge_id)
-    end
+      @challenges = Challenge.order(id: :asc).includes(:progresses)
   end
 
   def show
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -19,6 +19,6 @@ class Challenge < ApplicationRecord
   validates :title, presence: true
   validates :theme, presence: true
   validates :question, presence: true
-  has_many :complete_challenges, dependent: :destroy
-  has_many :progresses, dependent: :destroy, as: :materiable
+
+  include ProgressMateriable
 end
