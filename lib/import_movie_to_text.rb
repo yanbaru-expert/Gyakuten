4edb8fa47240040756a40a97a060ce7a7349e85c
T.Tsujii <ttakehisabt@gmail.com>
2021-05-29 16:53:41 +0900

    動画教材からテキスト教材へのインポート方法を修正

--- lib/import_movie_to_text.rb
+++ lib/import_movie_to_text.rb
@@ -1,10 +1,12 @@
 class ImportMovieToText
-  def execute(genre_id, index, base_image_name)
-    Movie.where(genre_id: genre_id).order(position: :asc).each.with_index(index) do |movie, i|
+  def self.execute(code_name, index)
+    genre = Genre.find_by(code_name: code_name)
+    Movie.where(genre_id: genre.id).order(position: :asc).each.with_index(index) do |movie, i|
       movie_params = movie.attributes.slice("title", "genre_id")
+      image_path = "#{Rails.root}/db/fixtures/#{code_name}/#{i.to_s.rjust(2, "0")}.jpg"
       add_params = {
         "content" => "",
-        "image" => open("#{Rails.root}/db/fixtures/#{i}_#{base_image_name}.jpg")
+        "image" => open(image_path),
       }
       text_params = movie_params.merge add_params
       text = Text.create!(text_params)
