4f6d98ecad511b1621e909d25dcd778caaac1fb0
OhtakeYusuke <o.yusuke0501@gmail.com>
2021-06-06 13:48:12 +0900

    お問い合わせメール送信後のリダイレクト先修正

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,13 @@ class ContactsController < ApplicationController
     ContactMailer.admin_email(@contact).deliver_now
     ContactMailer.user_email(@contact).deliver_now
     flash[:notice] = "メールの送信に成功しました。確認メールが届きます。"
-    redirect_to static_pages_url(anchor: "design_contact")
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:action] == "design"
+      redirect_to static_pages_url(anchor: "design_contact")
+      
+    else
+      redirect_to php_url(anchor: "php_contact")
+    end
   end
 
   private
@@ -19,7 +25,12 @@ class ContactsController < ApplicationController
     def contact_validation
       if params[:message].length > 1000 || params[:name].length > 100 || params[:ruby_name].length > 30
         flash[:notice] = "メールの送信に失敗しました。もう一度送信をお願いします"
-        redirect_to static_pages_url(anchor: "design_contact")
+        path = Rails.application.routes.recognize_path(request.referer)
+        if path[:action] == "design"
+          redirect_to static_pages_url(anchor: "design_contact")
+        else
+          redirect_to php_url(anchor: "php_contact")
+        end
       end
     end
 end
