c9567d54434f29c8d7672d22c54357a03328128b
T.Tsujii <ttakehisabt@gmail.com>
2021-06-08 10:58:17 +0900

    視聴済み読破済みなどのテーブルを削除

---
+++ db/migrate/20210608015220_drop_read_watch_tables.rb
@@ -0,0 +1,32 @@
+class DropReadWatchTables < ActiveRecord::Migration[5.2]
+  def up
+    remove_index :read_texts, [:user_id, :text_id]
+    remove_index :watched_movies, [:user_id, :movie_id]
+    drop_table :read_texts
+    drop_table :watched_movies
+    drop_table :complete_challenges
+  end
+
+  def down
+    create_table :read_texts do |t|
+      t.integer :user_id, null: false
+      t.integer :text_id, null: false
+
+      t.timestamps
+    end
+    create_table :watched_movies do |t|
+      t.integer :user_id, null: false
+      t.integer :movie_id, null: false
+
+      t.timestamps
+    end
+    create_table :complete_challenges do |t|
+      t.references :user, foreign_key: true
+      t.references :challenge, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :watched_movies, [:user_id, :movie_id], unique: true
+    add_index :read_texts, [:user_id, :text_id], unique: true
+  end
+end
