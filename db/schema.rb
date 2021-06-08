7c6847e4c5fda3a25c9fb0a87d908d2fc03fe8c9
T.Tsujii <ttakehisabt@gmail.com>
2021-06-08 17:54:25 +0900

    投稿のみとフル権限を分けられるようにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_025726) do
+ActiveRecord::Schema.define(version: 2021_06_08_072209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -182,6 +182,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_025726) do
     t.bigint "genre_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
     t.index ["genre_id"], name: "index_user_roles_on_genre_id"
     t.index ["user_id", "genre_id"], name: "index_user_roles_on_user_id_and_genre_id", unique: true
     t.index ["user_id"], name: "index_user_roles_on_user_id"
