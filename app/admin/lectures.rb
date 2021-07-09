07fdd965b4b934bdc17cc416950eafe6b0a65049
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-10 02:52:04 +0900

    Lecure モデルに対応する ActiveAdmin のページをコマンドで作成

---
+++ app/admin/lectures.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Lecture do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :position, :name, :lp_url, :description
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:position, :name, :lp_url, :description]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
